9c9c89bd93dbada32d23a69b674151a329681177
Ritish <165473767+RitishKulkarni@users.noreply.github.com>
2024-04-03 14:53:36 +0530

    Delete Investments in 2016.sql

--- Investments in 2016.sql
+++
@@ -1,8 +0,0 @@
-SELECT ROUND(SUM(tiv_2016), 2) AS tiv_2016
-FROM (
-    SELECT tiv_2016, tiv_2015, lat, lon,
-           COUNT(*) OVER (PARTITION BY tiv_2015) AS tiv_2015_count,
-           COUNT(*) OVER (PARTITION BY lat, lon) AS city_count
-    FROM Insurance
-) AS Subquery
-WHERE tiv_2015_count > 1 AND city_count = 1;
